09d5981c52683961e42a3df561d91373f8f64f2d
rodrigobadin <r.badin@gmail.com>
2016-12-13 19:21:18 -0200

    SpaceGirl - part 1

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody2D rigidbody2D;
+
+     // Awake is called when the script instance is being loaded
+     void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
+    void FixedUpdate()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+
+        float xSpeed = xMove * speed;
+        float ySpeed = yMove * speed;
+
+        Vector2 newVelocity = new Vector2(xSpeed, ySpeed);
+
+        rigidbody2D.velocity = newVelocity;
+    }
+}
