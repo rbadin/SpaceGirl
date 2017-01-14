fe0eb3bd6c953f7e1522b4906a27503e56d3cd0c
rodrigobadin <r.badin@gmail.com>
2017-01-13 23:59:15 -0200

    SpaceGirl - Parte 7
    - Inventário

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,21 +7,23 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     private PlayerInventoryDisplay playerInventoryDisplay;
-    private int totalStars = 0;
+    private List<Pickup> inventory = new List<Pickup>();
     
     // Start is called just before any of the Update methods is called the first time
     void Start()
     {
         playerInventoryDisplay = GetComponent<PlayerInventoryDisplay>();
+        playerInventoryDisplay.OnChangeInventory(inventory);
     }
 
     // OnTriggerEnter2D is called when the Collider2D other enters the trigger (2D physics only)
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Star"))
+        if (collision.CompareTag("Pickup"))
         {
-            totalStars++;
-            playerInventoryDisplay.OnChangeStartTotal(totalStars);
+            Pickup item = collision.GetComponent<Pickup>();
+            inventory.Add(item);
+            playerInventoryDisplay.OnChangeInventory(inventory);
             Destroy(collision.gameObject);
         }
     }
--- Assets/Scripts/PlayerInventoryDisplay.cs
+++ Assets/Scripts/PlayerInventoryDisplay.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerInventoryDisplay : MonoBehaviour
 {
-    public Image iconStarsYellow;
+    public Text inventoryText;
 
-    public void OnChangeStartTotal(int startTotal)
+    internal void OnChangeInventory(List<Pickup> inventory)
     {
-        float newWidth = 100 * startTotal;
-        iconStarsYellow.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
+        // clear display
+        inventoryText.text = "";
+
+        //build up new set of items
+        string newInventoryText = "carrying: ";
+        int numItems = inventory.Count;
+        for (int i = 0; i < numItems; i++)
+        {
+            string description = inventory[i].description;
+            newInventoryText += " [" + description + "]";
+        }
+        if (numItems < 1) newInventoryText = "(empty inventory)";
+        {
+            // update screen display
+            inventoryText.text = newInventoryText;
+        }
     }
 }
